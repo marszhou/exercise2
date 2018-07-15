56b069a1ccf1e04d8cec6c06e75e0f9b97dfc3fb
matt zhou <matt@cruelyouth.com>
2018-07-15 09:53:51 +0800

    login / register

--- src/pages/Account/Login.jsx
+++ src/pages/Account/Login.jsx
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Login extends Component {
   render() {
     return (
       <div>
-        Login 如果已经登录成功再访问此页面则都跳转到LoginedPage
+        <h2>登录</h2>
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td>用户名</td>
+              <td>
+                <input type="text" ref={el => (this.username = el)} />
+              </td>
+            </tr>
+            <tr>
+              <td>密码</td>
+              <td>
+                <input type="password" ref={el => (this.password = el)} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>
+          <button type="button" onClick={() => {}}>
+            登录
+          </button>
+        </p>
       </div>
-    );
+    )
   }
 }
 
-export default Login;
+export default Login
--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,19 +1,51 @@
-import React, { Component } from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom'
-import Register from './Register';
-import Login from './Login';
+import React, { Component } from 'react'
+import { Route, Switch, Redirect, NavLink } from 'react-router-dom'
+import Register from './Register'
+import Login from './Login'
+
+const navDefaultStyle = {
+  color: 'blue',
+  textDecoration: 'underline'
+}
+const navActiveStyle = {
+  color: 'black',
+  textDecoration: 'none'
+}
 class Account extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route path='/account/register' component={Register}/>
-          <Route path='/account/login' component={Login}/>
-          <Redirect to='/account/login' />
+          <Route path="/account/register" component={Register} />
+          <Route path="/account/login" component={Login} />
+          <Redirect to="/account/login" />
         </Switch>
+
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 10,
+          }}
+        >
+          <NavLink
+            to="/account/login"
+            style={navDefaultStyle}
+            activeStyle={navActiveStyle}
+          >
+            登录
+          </NavLink>{' '}
+          |{' '}
+          <NavLink
+            to="/account/register"
+            style={navDefaultStyle}
+            activeStyle={navActiveStyle}
+          >
+            注册
+          </NavLink>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default Account;
+export default Account
