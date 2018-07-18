fd6160fc5cdf891c7c05d7c16124c1f8a3dcd928
matt zhou <matt@cruelyouth.com>
2018-07-18 15:32:08 +0800

    DELETE BLOG

--- server/http/blog.js
+++ server/http/blog.js
@@ -1,6 +1,22 @@
 const { requireLogin } = require('./middlewares')
 
 module.exports = (app, db) => {
+  const validBlogRequest = async (req, res, next) => {
+    const blogId = req.params.blogId
+    const userId = req.login.id
+    const blog = await db.blog.get(blogId)
+    if (!blog) {
+      return res
+        .status(500)
+        .json({ code: 'BLOG_NOT_EXISTS', msg: 'blog不存在' })
+    }
+    if (blog.user_id !== userId) {
+      return res.status(500).json({ code: 'HAS_NO_RIGHTS', msg: '权限错误' })
+    }
+
+    next()
+  }
+
   app.get('/blogs/:blogId', async (req, res) => {
     const blog = await db.blog.get(req.params.blogId)
     res.json(blog)
@@ -22,19 +38,26 @@ module.exports = (app, db) => {
     res.json(ret)
   })
 
-  app.put('/blogs/:blogId', requireLogin(db), async (req, res) => {
-    const blogId = req.params.blogId
-    const userId = req.login.id
-    const { title, content } = req.body
+  app.put(
+    '/blogs/:blogId',
+    requireLogin(db),
+    validBlogRequest,
+    async (req, res) => {
+      const blogId = req.params.blogId
+      const { title, content } = req.body
+      const ret = await db.blog.update(blogId, { title, content })
+      res.json(ret)
+    }
+  )
 
-    const blog = await db.blog.get(blogId)
-    if (!blog) {
-      res.status(500).json({ code: 'BLOG_NOT_EXISTS', msg: 'blog不存在' })
+  app.delete(
+    '/blogs/:blogId',
+    requireLogin(db),
+    validBlogRequest,
+    async (req, res) => {
+      const blogId = req.params.blogId
+      const ret = await db.blog.delete(blogId)
+      res.json(ret)
     }
-    if (blog.user_id !== userId) {
-      res.status(500).json({ code: 'HAS_NO_RIGHTS', msg: '权限错误' })
-    }
-    const ret = await db.blog.update(blogId, { title, content })
-    res.json(ret)
-  })
+  )
 }
